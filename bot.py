99c66d4a99a264955199202cca8b549a74576134
rezaizadijahromi <rezaizadij2000@gmail.com>
2021-11-11 22:34:09 +0330

    Can download private page contents

--- bot.py
+++ bot.py
@@ -1,9 +1,16 @@
+from typing import Counter
+from requests.models import CONTENT_CHUNK_SIZE, iter_slices
 from selenium import webdriver
+from selenium.webdriver.common import keys
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
 from time import sleep
+import requests
+import os
+from urllib.parse import urlparse
 
 from secret import pw
 
@@ -12,13 +19,13 @@ class Bot:
     links = []
 
     def __init__(self):
-        self.login_by_xpath('rezaizadi_ij', pw)
+        self.login_by_xpath('username', pw)
         # self.login('rezaizadi_ij', pw)
 
         sleep(2)
 
         # go to page
-        self.go_to_page('')
+        self.go_to_page('page you want')
 
     def login_by_xpath(self, username, password):
         self.driver = webdriver.Chrome("D:/Servers dj/insta_scraping/chromedriver.exe")
@@ -32,7 +39,7 @@ class Bot:
         password_input = self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input')
         password_input.send_keys(password)
 
-        sleep(1)
+        sleep(3)
 
 # /html/body/div[4]/div/div/button[1]
         self.driver.find_element_by_xpath("//button[contains(text(), 'Accept All')]").click() # clicking 'not now btn'
@@ -44,39 +51,62 @@ class Bot:
         self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]").click() # clicking 'not now btn'
         sleep(3)
         self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]").click() 
-        # sleep(3)
-        # self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]").click()
-
-        # sleep(3)
-
-        # page = self.driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input')
-        # page.send_keys('deutsch_cafe_a1_b1')
+     
 
 
 
     def go_to_page(self, page):
         print("Now we are here")
 
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        data_dir = os.path.join(base_dir, "data")
+        os.makedirs(data_dir, exist_ok=True)
+
         self.driver.get(f"https://www.instagram.com/{page}/channel")
 
         sleep(2)
         
-        all_videos_links = self.driver.find_element_by_tag_name("a")
+        links = self.driver.find_elements_by_tag_name("a")
 
         def condition(links):
-            return ".com/tv" in links.get_attribute('href')
+            link =  ".com/tv/" in links.get_attribute('href')
+            return link
 
-        valid_video_links = list(filter(condition, all_videos_links))
+        valid_video_links = list(filter(condition, links))
 
-        for i in range(9):
+
+        for i in range(1):
             link = valid_video_links[i].get_attribute('href')
-            if link not in self.all_videos_links:
-                self.valid_video_links.append(link)
+            if link not in self.links:
+                self.links.append(link)
 
-        print(len(valid_video_links))
-        print("all the href are", valid_video_links)
-        # for link in all_videos_links:
-        #     self.get(link)
+        for link in self.links:
+            self.driver.get(link)
+            video = self.driver.find_element_by_tag_name('video')
+            url = video.get_attribute('src')
+            base_url = urlparse(url).path
+            filename = os.path.basename(base_url)
+            filepath = os.path.join(data_dir, filename)
+
+            if os.path.exists(filepath):
+                continue
+            with requests.get(url, stream=True) as r:
+                try:
+                    r.raise_for_status()
+                except:
+                    continue
+                with open(filepath, 'wb') as f:
+                    for chunk in r.iter_content(chunk_size=8192):
+                        if chunk:
+                            f.write(chunk)
+
+
+            #To like it
+            # self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div[1]/article/div/div[3]/div/div/section[1]/span[1]/button').click()
+
+
+
+            
 
 
 
@@ -104,3 +134,16 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+   # sleep(3)
+        # self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]").click()
+
+        # sleep(3)
+
+        # page = self.driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input')
+        # page.send_keys('deutsch_cafe_a1_b1')
